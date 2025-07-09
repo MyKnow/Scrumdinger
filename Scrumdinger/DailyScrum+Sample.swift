0a2fa90c36a31119b98c125c3be945226433e423
MyKnow <myknow000@gmail.com>
2025-07-09 19:00:12 +0900

    feat: Stylize the CardView

--- Scrumdinger/DailyScrum+Sample.swift
+++ Scrumdinger/DailyScrum+Sample.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import ThemeKit
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] = [
+        DailyScrum(title: "Design", attendees: ["홍길동", "장발장", "정민호"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["팀쿡", "잡스", "정민호"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["이재용", "김철수", "이영희"], lengthInMinutes: 5, theme: .poppy),
+    ]
+}
