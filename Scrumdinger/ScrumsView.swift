c10132e0e6563b1a0bd3c8e8f7485f80916c7457
MyKnow <myknow000@gmail.com>
2025-07-09 19:17:41 +0900

    feat: Display a list of daily scrums

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by MyKnow on 7/9/25.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
