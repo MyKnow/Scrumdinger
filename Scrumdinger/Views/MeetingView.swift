2ed8ea16b323001b31ec6e862ad509882e130f9c
MyKnow <myknow000@gmail.com>
2025-07-12 10:50:10 +0900

    feat: Integrate header and add life cycle in MeetingView

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 import ThemeKit
+import TimerKit
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @State var scrumTimer = ScrumTimer()
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining,
+                    theme: scrum.theme
+                )
                 Circle()
                     .strokeBorder(lineWidth: 24)
                 HStack {
@@ -29,6 +36,16 @@ struct MeetingView: View {
         }
         .padding()
         .foregroundStyle(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(
+                lengthInMinutes: scrum.lengthInMinutes,
+                attendeeNames: scrum.attendees.map { $0.name }
+            )
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
         .navigationBarTitleDisplayMode(.inline)
     }
 }
